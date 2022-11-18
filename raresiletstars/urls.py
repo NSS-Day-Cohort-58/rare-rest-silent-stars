e5dc8b62a88efea19a95960191ad2f5a2360172f
Adam Featherston <j.adam.featherston@gmail.com>
2022-11-18 15:59:40 -0600

    category model and view

--- raresiletstars/urls.py
+++ raresiletstars/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from raresiletstars.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from raresiletstars.views import CategoryView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
