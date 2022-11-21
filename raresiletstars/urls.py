2d614c0cd05859a2b50f16dd6a47dfaa3ba50650
shelbyjjoiner <shelbyjjoiner@yahoo.com>
2022-11-21 09:50:18 -0600

    added post view,model, and updated url

--- raresiletstars/urls.py
+++ raresiletstars/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from rareapi.views import PostView
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
