9308d3eab033e1e5f007310611dc1fc4a336673b
shelbyjjoiner <shelbyjjoiner@yahoo.com>
2022-11-21 10:04:05 -0600

    changed post model

--- rareapi/models/posts.py
+++ rareapi/models/posts.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     tags =  models.ManyToManyField("Tags", through="PostTags")
-    category = models.IntegerField()
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete =models.CASCADE)
     title = models.CharField(max_length=24)
     image_url = models.CharField()
     content = models.CharField()
