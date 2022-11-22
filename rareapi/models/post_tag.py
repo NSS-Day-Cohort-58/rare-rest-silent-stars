bfd866e043e2f16ce3588c29c5a7671e278c6afe
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-11-22 10:34:19 -0500

    modifications to test the code, modified posttag, posts and created a rareuser file

--- rareapi/models/post_tag.py
+++ rareapi/models/post_tag.py
@@ -3,4 +3,4 @@ from django.db import models
 class PostTag(models.Model): 
 
     post = models.ForeignKey("Post", null=True, blank=True, on_delete =models.CASCADE)
-    tag = models.ForeignKey("Tags", null=True, blank=True, on_delete =models.CASCADE)
+    tag = models.ForeignKey("Tag", null=True, blank=True, on_delete =models.CASCADE)
--- rareapi/models/rare_user.py
+++ rareapi/models/rare_user.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class RareUser(models.Model): 
 
     bio = models.CharField(max_length=24)
-    profile_image = models.CharField()
-    user= models.ForeignKey("User", on_delete =models.CASCADE)
+    profile_image = models.CharField(max_length=400)
+    user= models.ForeignKey(User, on_delete =models.CASCADE)
