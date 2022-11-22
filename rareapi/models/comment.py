6f52351f378bac3d6aaf2ef910425dc38bdbf77b
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-11-22 11:04:10 -0500

    one file

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class Comment(models.Model): 
     author = models.OneToOneField('RareUser', on_delete=models.CASCADE)
     post = models.ForeignKey('Post', null=True, blank=True, on_delete =models.CASCADE)
-    content = models.CharField()
+    content = models.CharField(max_length=300)
